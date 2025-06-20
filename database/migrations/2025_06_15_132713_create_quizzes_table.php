0f3c177e30daf9de49b1d0eb4d124f11a2e89443
octavia nur aisya <octavianuraisya@gmail.com>
2025-06-20 23:13:21 +0700

    migrate

--- database/migrations/2025_06_15_132713_create_quizzes_table.php
+++ database/migrations/2025_06_15_132713_create_quizzes_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-    {
+    { 
         Schema::create('quizzes', function (Blueprint $table) {
        $table->id();
        $table->foreignId('course_id')->constrained()->onDelete('cascade');
